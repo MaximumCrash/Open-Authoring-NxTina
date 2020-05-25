084f4c45d6907682c43cca1d4a617160bba594e1
Rejon <rejon@maximumcrash.com>
2020-05-25 11:19:00 -0400

    Commiting so we can update Github to show me proper connection.

--- pages/index.tsx
+++ pages/index.tsx
@@ -207,3 +207,31 @@ export default function Home() {
     </div>
   )
 }
+
+
+/**
+ * Fetch data with getStaticProps based on 'preview' mode
+ */
+ export const getStaticProps: GetStaticProps = async function({
+  preview,
+  previewData,
+ }) {
+  if (preview) {
+    return getGithubPreviewProps({
+      ...previewData,
+      fileRelativePath: 'content/home.json',
+      parse: parseJson,
+    })
+  }
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: 'content/home.json',
+        data: (await import('../content/home.json')).default,
+      },
+    },
+  }
+ }
